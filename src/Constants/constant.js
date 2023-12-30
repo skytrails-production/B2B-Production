5d12aba208925d6f0657fbf83c6e047e778908bc
Qamar Ali <qamarali205@gmail.com>
2023-12-30 15:41:08 +0530

    updated

--- src/Constants/constant.js
+++ src/Constants/constant.js
@@ -2,10 +2,10 @@ exports.apiURL = {
   // baseURL: "https://api.skyTrails.com",
   // baseURL: "https://stagingapi.easemytrip.com",
 
-  // baseURL:"https://back.theskytrails.com",
-  baseURL: "http://localhost:8000",
+  baseURL:"https://back.theskytrails.com",
+  // baseURL: "http://localhost:8000",
 
-  // flightEmail: "vivekguptatekari@gmail.com",
-  // phoneNo: "9135219071",
+  flightEmail: "vivekguptatekari@gmail.com",
+  phoneNo: "9135219071",
 
 };
