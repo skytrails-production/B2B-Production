36d94deb56aea8ee811c43d2fb6f65476d33c22b
shivam singh <Shivam.11506226@lpu.in>
2024-01-09 11:28:36 +0530

    Update constant.js

--- src/Constants/constant.js
+++ src/Constants/constant.js
@@ -2,8 +2,8 @@ exports.apiURL = {
   // baseURL: "https://api.skyTrails.com",
   // baseURL: "https://stagingapi.easemytrip.com",
 
-  baseURL:"https://back.theskytrails.com",
-  // baseURL: "http://localhost:8000",
+  //baseURL:"https://back.theskytrails.com",
+  baseURL: "http://localhost:8000",
 
   flightEmail: "vivekguptatekari@gmail.com",
   phoneNo: "9135219071",
