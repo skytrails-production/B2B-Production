0926950976d932222f09b3c67908615e4add0927
Qamar Ali <qamarali205@gmail.com>
2025-03-10 11:39:25 +0530

    update code

--- src/Constants/constant.js
+++ src/Constants/constant.js
@@ -3,6 +3,7 @@ exports.apiURL = {
   // baseURL: "https://tourfinderbackend-production.up.railway.app",
 
    baseURL: "https://back.theskytrails.com",
+   
 
   // baseURL: "http://localhost:8000",
   flightEmail: "shivam@theskytrails.com",
